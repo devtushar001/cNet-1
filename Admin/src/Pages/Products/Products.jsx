ee5f6a599045da9f7517d6366d070c153d484aa1
devtushar001 <tc341269@gmail.com>
2025-02-24 18:34:15 +0530

    something completed

--- Admin/src/Pages/Products/Products.jsx
+++ Admin/src/Pages/Products/Products.jsx
@@ -5,21 +5,28 @@ import './Products.css';
 
 const Products = () => {
     const { catImage, setCatImage } = useContext(TShakyaContext);
+    const selection = "category";
     return (
         <>
             <div className="product-container">
                 <div className="add-category">
-                    <img style={{ height: "270px" }} src={catImage} alt="" />
+                    <h2 style={{ marginLeft: "30px", padding: "10px", borderBottom: "1px solid gray" }}>Add Category</h2>
+                    <div className="img-box">
+                        <img style={{ height: "270px" }} src={catImage} alt="" />
+                        {!catImage ? "" : <button onClick={() => setCatImage("")} style={{ height: "40px", cursor: "pointer", borderRadius: "50%" }}>Remove</button>}
+                    </div>
                     <div className="action">
                         <input placeholder="Enter shop category name" type="text" />
                         <button>Submit</button>
                     </div>
                 </div>
-
                 <br />
                 <hr />
                 <br />
-                <ImageUploader />
+                <div className="add-product">
+                    <h2 style={{ marginLeft: "30px", padding: "10px", borderBottom: "1px solid gray" }}>Add Product</h2>
+                </div>
+                <ImageUploader selection={selection} />
             </div>
         </>
     )
